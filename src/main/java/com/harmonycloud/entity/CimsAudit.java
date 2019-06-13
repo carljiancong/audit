7915e1ab980245508d2789a21d86a4abd58c48a2
jiancongc <carl.chen@asiasoft.hk>
2019-06-13 17:10:51 +0800

    commit.

--- src/main/java/com/harmonycloud/entity/CimsAudit.java
+++ src/main/java/com/harmonycloud/entity/CimsAudit.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Table(name = "cims_audit")
 public class CimsAudit {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer auditId;
     @Column(name = "audit_date")
     private Date auditDate;
